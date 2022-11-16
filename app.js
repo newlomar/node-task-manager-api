b7bb97d30e98ed30b2ed533764909b96bea87c93
Newton Lomar <newtonlomar16@gmail.com>
2022-11-16 19:54:41 -0300

    adding json middleware

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
+
+
+// middleware
+
+app.use(express.json())
 
 // routes
-
 app.get('/hello', (req, res) => {
     res.send('Task Manager App')
 })
 
+app.use('/api/v1/tasks', tasks)
 
 // app.get('/api/v1/tasks')          - get all the tasks
 // app.post('/api/v1/tasks')         - create a new task
