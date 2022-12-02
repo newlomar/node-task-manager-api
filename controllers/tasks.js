c56b69635310bd9f2c44051793970d8a4d564781
Newton Lomar <newtonlomar16@gmail.com>
2022-12-02 20:31:34 -0300

    improving delete controller to actually delete a task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -36,8 +36,18 @@ const updateTask = (req, res) => {
     res.json({id: req.params.id})
 }
 
-const deleteTask = (req, res) => {
-    res.send({id: req.params.id})
+const deleteTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndDelete({_id: taskID})
+        if (!task) {
+            return res.status(404).json({ message: `No task with id: ${taskID}` })
+        }
+
+        res.status(200).json({ task: null, status: 'success' })
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 }
 
 module.exports = {
