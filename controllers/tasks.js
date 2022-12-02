25682503d06e80f0388c48a77b39bccfda901935
Newton Lomar <newtonlomar16@gmail.com>
2022-12-02 20:22:49 -0300

    adding response to when there is no task with the given id

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -22,12 +22,14 @@ const getTask = async (req, res) => {
     try {
         const {id: taskID} = req.params
         const task = await Task.findOne({ _id: taskID })
+        if (!task) {
+            return res.status(404).json({ message: `No task with id: ${taskID}` })
+        }
 
         res.status(200).json({ task })
     } catch (error) {
         res.status(500).json({ message: error })
     }
-    res.json({id: req.params.id})
 }
 
 const updateTask = (req, res) => {
