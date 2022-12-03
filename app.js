9cd5f7cdb1ad12b104154c185deed5f7202e5cb8
Newton Lomar <newtonlomar16@gmail.com>
2022-12-02 23:04:55 -0300

    changing env variable name

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 3001
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI)
+        await connectDB(process.env.MONGO_URL)
         app.listen(port, console.log(`server is listening on port ${port}...`))
     } catch (error) {
         console.log(error)
