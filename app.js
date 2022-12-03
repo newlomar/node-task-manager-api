8a17a6d4e1adfcb76b900817ec74878c4304c205
Newton Lomar <newtonlomar16@gmail.com>
2022-12-02 21:21:23 -0300

    clean up

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ app.use(express.json())
 // routes
 app.use('/api/v1/tasks', tasks)
 
-// app.get('/api/v1/tasks')          - get all the tasks
-// app.post('/api/v1/tasks')         - create a new task
-// app.get('/api/v1/tasks/:id')      - get a single task
-// app.patch('/api/v1/tasks/:id')    - update task
-// app.delete('/api/v1/tasks/:id')   - delete task
 
 const port = 3000
 
