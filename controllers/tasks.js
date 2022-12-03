53472bad017f6e2990ba895b6309eeb4babe91c4
Newton Lomar <newtonlomar16@gmail.com>
2022-12-02 22:10:32 -0300

    finished resolving modular error handle

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,6 @@
 const Task = require('../models/Task');
 const asyncWrapper = require('../middleware/async')
-
+const { createCustomError } = require('../errors/custom-error')
 const getAllTasks = asyncWrapper(async (req, res) => {
         const tasks = await Task.find({})
         // other response options
@@ -19,10 +19,7 @@ const getTask = asyncWrapper(async (req, res, next) => {
     const task = await Task.findOne({ _id: taskID })
     
     if (!task) {
-        const error = new Error('Not Found')
-        error.status = 404
-        return next(error)
-        return res.status(404).json({ message: `No task with id: ${taskID}` })
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
     }
     res.status(200).json({ task })
 })
@@ -35,7 +32,7 @@ const updateTask = asyncWrapper(async (req, res) => {
     })
 
     if (!task) {
-        return res.status(404).json({ message: `No task with id: ${taskID}` })
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
     }
 
     res.status(200).json({ task })
@@ -46,7 +43,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
     const task = await Task.findOneAndDelete({_id: taskID})
     
     if (!task) {
-        return res.status(404).json({ message: `No task with id: ${taskID}` })
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
     }
 
     res.status(200).json({ task: null, status: 'success' })
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,9 @@
+const { CustomAPIError } = require('../errors/custom-error')
 const errorHandlerMiddleware = (err, req, res, next) => {
-    return res.status(err.status).json({ message: err.message })
+    if (err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json({ message: err.message})
+    }
+    return res.status(500).json({ message: 'Something went wrong, try again later or contact support' })
 }
 
 module.exports = errorHandlerMiddleware
